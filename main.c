57aa518765a4e75f187d9b4805dd48127caab9ed
TenMileJones <tenmilejones@gmail.com>
2024-01-30 03:57:25 +0000

    fix2

--- main.c
+++ main.c
@@ -8,16 +8,11 @@ int main(int argc, char *argv[])
 {
 	Employee A[MAX_EMPLOYEES];
 	for(int i = 0; i < MAX_EMPLOYEES; i++){
-		char str[MAX_NAME_LEN];
-		int money = 0;
 		Employee e;
 		printf("\n\nName: ");
-        scanf("%s", str);
+        scanf("%s", e.name);
 		printf("Salary: ");
-		scanf("%d", &money);
-		
-		e.name[MAX_NAME_LEN] = *str;
-		e.salary = money;
+		scanf("%d", &e.salary);
 		A[i] = e;
 	}
 
