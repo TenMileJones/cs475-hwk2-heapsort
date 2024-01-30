932a33846192751a8d4ae35872c2fb4d825f4be4
TenMileJones <tenmilejones@gmail.com>
2024-01-30 03:39:34 +0000

    main() attempt 1

--- main.c
+++ main.c
@@ -2,9 +2,26 @@
 #include "employee.h"
 #include "heap.h"
 
+#define MAX_EMPLOYEES 5
+
 int main(int argc, char *argv[])
 {
-	//TODO
+	Employee A[MAX_EMPLOYEES];
+	for(int i = 0; i < MAX_EMPLOYEES; i++){
+		char str[MAX_NAME_LEN];
+		int money = 0;
+		printf("\n\nName: ");
+        scanf("%s", str);
+		printf("\nSalary: ");
+		scanf("%d", money);
+		Employee e;
+		e.name[MAX_NAME_LEN] = str;
+		e.salary = money;
+		A[i] = e;
+	}
+
+	heapSort(A, MAX_EMPLOYEES);
+	printList(A, MAX_EMPLOYEES);
 	
 	return 0;
 }
